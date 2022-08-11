ecf78167950281177ffc9bdcc256e7d760f19bd4
Jakub <nastosk8@egmail.com>
2022-08-11 22:25:34 +0200

    commit please

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,95 @@
-function Ratajvek() {
-    CurrentYear = Number(2022)
-    tvojvek = Number(prompt("Kolko mas rokov?"))
-    var Birth = CurrentYear - tvojvek;
-    console.log("Narodil si sa v " + Birth);
-    document.getElementById("Birth").innerHTML = Birth;
-    planoletost = tvojvek >= 18 ? (planoletost = "Si plnolety") : (planoletost = "Nie si plnolety")
-    document.getElementById("Plnoletost").innerHTML = planoletost;
-    console.log(planoletost)
+///// ***** Left Menu Panel ***** /////////
+var button = document.getElementsByClassName('LeftMenuShowBtn');
+button[0].addEventListener('mousemove', ButtonSizeUp);
+button[0].addEventListener('mouseleave', ButtonSizeDown);
+button[0].addEventListener('click', ShowHideLeftMenu);
+var LeftMenuIndexVar = 1;
+var LeftMenuOptions = ['Pricing', 'About Us', 'Services', 'Reference'];
+var LeftMenuList = document.getElementById('LeftMenutext');
+console.log(LeftMenuList);
+
+function ButtonSizeUp(){
+    button[0].style.fontSize = '220%';
 };
-var y = Number(-1);
-var x = Number(0);
-var TriggerWhile = Number(1);
-var text = document.getElementsByClassName("P1");
-var textStyleBig = "340%";
-var textStyleSmall = "270%";
-console.log(text.length);
-console.log(text);
 
-function NastavText(){
-    console.log(text.length)
-    for (var i = 1; i < text.length + 1; i++){
-        console.log(i)
-        if (Trigger == 1 && y == -1){
-            console.log(y)
-            text[i - 1].style.fontSize = "480%";
-            console.log("Nastavujem" + x + ". pismeno.");
-            break;
-        }else if (Trigger == 1){
-            text[i - 1].style.fontSize = "480%";
-            console.log("Nastavujem" + x + ". pismeno.");
-            break;
-        }else {
-            Ukoncit()
-            console.log("Ukoncujem")
+function ButtonSizeDown(){
+    button[0].style.fontSize = '170%';
+};
+
+function ShowHideLeftMenu(){
+    if (LeftMenuIndexVar == 1){
+        ShowLeftMenu();
+        console.log('Pustil som Show Menu Bar')
+        console.log(LeftMenuIndexVar)
+        LeftMenuIndexVar = 0;
+        console.log(LeftMenuIndexVar)
+    }else {
+        HideLeftMenu();
+        console.log('Pustil som Hide Menu Bar');
+        LeftMenuIndexVar = 1;
+    }
+};
+
+function ShowLeftMenu(){
+    for (var i = 0; i < LeftMenuOptions.length - 1;i++){
+    var LeftMenuOptionsVar = document.createElement('li');
+    LeftMenuOptionsVar.textContent = LeftMenuOptions[i];
+    LeftMenuList.appendChild(LeftMenuOptionsVar);
+    
+    }
+}
+
+function HideLeftMenu (){
+    for (var i = 0; i < LeftMenuOptions.length - 1;i++){
+        var DeleteMenuList = document.getElementsByTagName('li');
+        console.log(DeleteMenuList);
+        LeftMenuList.removeChild(DeleteMenuList[0]);
         }
-    }
 }
+/////// Konec Left Menu Panel ***** ///////
+///////////////////////////////////////////
 
-function Ukoncit(){
-    for (var i = 0; i < text.length; i++){
-        text[i].style.fontSize = "320%";
+
+//////// ***** Meni Text ***** /////////
+// * Pole s textom* //
+var PohiblivyTextPole = [
+'<p class="P1">C</p><p class="P1">O</p><p class="P1">N</p><p class="P1">S</p><p class="P1">U</p><p class="P1">L</p><p class="P1">T</p><p class="P1">I</p><p class="P1">N</p><p class="P1">G</p>', 
+'<p class="P1">N</p><p class="P1">E</p><p class="P1">T</p><p class="P1">W</p><p class="P1">O</p><p class="P1">R</p><p class="P1">K</p>',
+'<p class="P1">C</p><p class="P1">L</p><p class="P1">O</p><p class="P1">U</p><p class="P1">D</p>',
+'<p class="P1">A</p><p class="P1">P</p><p class="P1">P</p><p class="P1">L</p><p class="P1">I</p><p class="P1">C</p><p class="P1">A</p><p class="P1">T</p><p class="P1">I</p><p class="P1">O</p><p class="P1">N</p><p class="P1">S</p>'
+];
+
+var PohiblivyText = document.getElementsByClassName("PohiblivyText");
+console.log(PohiblivyText[0].innerHTML);
+console.log(PohiblivyText.length);
+var PohiblivyTextVar = 0;
+var PismenkaVar = 0;
+
+setInterval( function (){
+    if (PohiblivyTextVar > -1){
+        console.log("Pustam zmenu textu")
+        ZamenaTextov()        
     }
+}, 10000);
+
+function ZamenaTextov(){
+    if (PohiblivyTextVar < PohiblivyTextPole.length){
+    PohiblivyText[0].innerHTML = PohiblivyTextPole[PohiblivyTextVar]
+    console.log(PohiblivyText[0].innerHTML)
+    PohiblivyTextVar++
+    setInterval( function (){
+        var Pismenka = document.getElementsByClassName("P1");
+            if (PismenkaVar < Pismenka.length){
+            Pismenka[PismenkaVar].style.fontSize = "420%"
+            PismenkaVar++
+            }
+            }, 800)
+    PismenkaVar = 0;
+}else {
+    PohiblivyTextVar = 0
+    PismenkaVar = 0;
 }
-
-
-
+};
+/////// **** KONCI Zmena Text **** ///////////////
 
 
--- scripts/vsescript.js
+++ scripts/vsescript.js
@@ -2,21 +2,48 @@ function Vek(rok){
     return 2022 - rok
 };
 
-function BackroundChange(BackroundTrigger){
-console.log(BackroundTrigger);
+var Pictures = ["url(/funpage/images/backround1.png)", "url(/funpage/images/backround2.png)","url(/funpage/images/backround3.png)","url(/funpage/images/backround4.png"];
+var totalCount = Pictures.length;
+var currentIndex = 0;
 
-if (BackroundTrigger == "1"){
-    document.body.style.backgroundImage = "url(/funpage/images/backround2.png)"
-}else if(BackroundTrigger == "2"){
-    document.body.style.backgroundImage = "url(/funpage/images/backround1.png)"
-}else {
-    console.log("Hovno")
-};
-};
+setInterval(function() {
+      if (currentIndex < totalCount){
+      document.body.style.backgroundImage = Pictures[currentIndex]
+      currentIndex++}
+  }, 5000);
 
+  
+setInterval( function (){
+    if (currentIndex = totalCount){
+        currentIndex = 0
+    }
+}, 21000);
 
-function delay(i) {
-    setTimeout(() => {
-      console.log(i)
-    }, 1000);
-  }
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
